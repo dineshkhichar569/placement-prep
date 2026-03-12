c6460b0726b72c2f262b1af6d40b7638cf570da8
dineshkhichar569 <dineshkhichar569@gmail.com>
2026-03-13 03:00:19 +0530

    example

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static int add(int x, int y) {
+        int sum = x + y;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int result = add(a, b);
+        System.out.println(result);
+    }
+}
