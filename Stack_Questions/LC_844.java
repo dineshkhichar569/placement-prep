ceb3ca6fa128c395c97d99b0061173e718e5a05e
dineshkhichar569 <dineshkhichar569@gmail.com>
2026-04-03 19:21:04 +0530

    leetcode 844 solved using stack

---
+++ Stack_Questions/LC_844.java
@@ -0,0 +1,34 @@
+package Stack_Questions;
+
+import java.util.Stack;
+
+public class LC_844 {
+    public static boolean backspaceCompare(String s, String t) {
+        Stack<Character> s1 = new Stack<>();
+        Stack<Character> t1 = new Stack<>();
+
+        for (char i : s.toCharArray()) {
+            if (i == '#' && !s1.isEmpty()) {
+                s1.pop();
+            } else if (i != '#') {
+                s1.push(i);
+            }
+        }
+        for (char i : t.toCharArray()) {
+            if (i == '#' && !t1.isEmpty()) {
+                t1.pop();
+            } else if (i != '#') {
+                t1.push(i);
+            }
+        }
+
+        return s1.equals(t1);
+    }
+
+    public static void main(String[] args) {
+        String s = "ab#c";
+        String t = "ac#c";
+
+        System.out.println(backspaceCompare(s, t));
+    }
+}
