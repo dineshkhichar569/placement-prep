08014180f42729c866f2b542845a3a1ec76a9360
dineshkhichar569 <dineshkhichar569@gmail.com>
2026-04-03 18:05:33 +0530

    optimized solution of leetcode 682

--- Stack_Questions/LC_682.java
+++ Stack_Questions/LC_682.java
@@ -7,27 +7,20 @@ public class LC_682 {
         Stack<Integer> stack = new Stack<>();
 
         for (String i : operations) {
-            if (i.matches(".*\\d.*")) {
-                stack.push(Integer.valueOf(i));
-            } else if (i.equals("+") && !stack.isEmpty()) {
-                int a = stack.pop();
-                int b = stack.pop();
-                int sum = a + b;
-                stack.push(b);
-                stack.push(a);
-                stack.push(sum);
+            if (i.equals("+")) {
+                stack.push(stack.peek() + stack.get(stack.size() - 2));
             } else if (i.equals("D")) {
                 int d = 2 * stack.peek();
                 stack.push(d);
-            } else if (i.equals("C") && !stack.isEmpty()) {
+            } else if (i.equals("C")) {
                 stack.pop();
+            } else {
+                stack.push(Integer.valueOf(i));
             }
         }
 
-        System.out.println(stack);
-
         int sum = 0;
-        while(!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             sum = sum + stack.pop();
         }
 
