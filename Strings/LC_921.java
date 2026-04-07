05021395c729e0d6d03dcd2ad2c1fe0d363d0aba
dineshkhichar569 <dineshkhichar569@gmail.com>
2026-04-07 14:49:45 +0530

    leetcode 921 solved using stack

--- Strings/LC_921.java
+++ Strings/LC_921.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class LC_921 {
     public static int valid(String s){
         int open = 0;
@@ -16,10 +18,34 @@ public class LC_921 {
         return open + close;
     }
 
+    ///////////////////////////////  Using Stack  ////////////////////////////////////////////
+    ///////////////////////////////  Using Stack  ////////////////////////////////////////////
+    ///////////////////////////////  Using Stack  ////////////////////////////////////////////
+
+    public static int minAddToMakeValid(String s){
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
+                stack.push(ch);
+            } else {
+                if(!stack.isEmpty() && stack.peek() == '('){
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            }
+        }
+
+        return stack.size();
+    }
+
     public static void main(String[] args) {
         // String s = "())";
         String s = ")((((";
 
         System.out.println(valid(s));
+        
+        System.out.println(minAddToMakeValid(s));
     }
 }
