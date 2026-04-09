e40e0805f6c6d4bec813b76417c75cb64ab68248
dineshkhichar569 <dineshkhichar569@gmail.com>
2026-04-10 01:09:23 +0530

    leetcode 227 solved using stack

--- Stack_Questions/LC_227.java
+++ Stack_Questions/LC_227.java
@@ -1,21 +1,52 @@
 package Stack_Questions;
 
+import java.util.Stack;
+
 public class LC_227 {
-    private static int calculate(int a, int b, String token) {
-        if (token.equals("+")) {
-            return a + b;
-        } else if (token.equals("-")) {
-            return a - b;
-        } else if (token.equals("/")) {
-            return a / b;
+    public static int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        s = s.replaceAll(" ", "");
+
+        char sign = '+';
+        int num = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                num = (ch - '0');
+            }
+
+            if (!Character.isDigit(ch) || i == s.length()-1) {                
+                if (sign == '+') {
+                    stack.push(num);
+                }
+                if (sign == '-') {
+                    stack.push(-num);
+                }
+                if (sign == '*') {
+                    stack.push(stack.pop() * num);
+                }
+                if (sign == '/') {
+                    stack.push(stack.pop() / num);
+                }
+
+                sign = ch;
+                num = 0;
+            }
         }
-        return a * b;
+
+        int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
         // String s = "3/2";
-        String s = " 3/ 2 ";
-        s = s.replaceAll(" ", "");
-        System.out.println(s.charAt(0) - '0');
+        String s = "3-2*2";
+        System.out.println(calculate(s));
     }
 }
