cbd6c97585d980bd13e9875210aa4f62b1db4df3
dineshkhichar569 <dineshkhichar569@gmail.com>
2026-04-16 02:20:11 +0530

    solved leetcode 104 it is a tree question

---
+++ Tree/LC_104.java
@@ -0,0 +1,28 @@
+package Tree;
+
+public class LC_104 {
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+
+    public static void main(String[] args) {
+        Binary_tree_root tree = new Binary_tree_root();
+
+        Integer[] nodes = { 3, 9, 20, null, null, 15, 7 };
+        tree.buildFromArray(nodes);
+
+        System.out.println("\nTree:");
+        tree.display();
+
+        int ans = maxDepth(tree.getRoot());
+
+        System.out.println("\nMax Depth = " + ans);
+    }
+}
