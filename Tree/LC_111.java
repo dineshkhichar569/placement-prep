8e38b2a5a3b5e64765b16ddce8ca4813b13b1a8c
dineshkhichar569 <dineshkhichar569@gmail.com>
2026-04-16 02:20:38 +0530

    tree question 111 of leetcode solved

---
+++ Tree/LC_111.java
@@ -0,0 +1,31 @@
+package Tree;
+
+public class LC_111 {
+    public static int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = minDepth(root.left);
+        int rightDepth = minDepth(root.right);
+
+        if(leftDepth == 0) return 1 + rightDepth;
+        if(rightDepth == 0) return 1 + leftDepth;
+
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+
+    public static void main(String[] args) {
+        Binary_tree_root tree = new Binary_tree_root();
+
+        Integer[] nodes = { 2,null,3,null,4,null,5,null,6 };
+        tree.buildFromArray(nodes);
+
+        System.out.println("\nTree:");
+        tree.display();
+
+        int ans = minDepth(tree.getRoot());
+
+        System.out.println("\nMin Depth = " + ans);
+    }
+}
