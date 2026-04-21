5ceb98b5cfb94a21a892029a51a9ed77e3624c0b
dineshkhichar569 <dineshkhichar569@gmail.com>
2026-04-22 01:32:46 +0530

    solvd letcode 121 using 3 approaches

---
+++ Kadanes_Algorithm/LC_121.java
@@ -0,0 +1,62 @@
+package Kadanes_Algorithm;
+
+public class LC_121 {
+    public static int maxProfit_1(int[] arr) {
+        int maxProfit = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    int profit = arr[j] - arr[i];
+                    maxProfit = profit > maxProfit ? profit : maxProfit;
+                }
+            }
+        }
+
+        if (maxProfit < 0) {
+            maxProfit = 0;
+        }
+
+        return maxProfit;
+    }
+
+    public static int maxProfit_2(int[] arr) {
+        int min = arr[0];
+        int maxProfit = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            min = Math.min(min, arr[i]);
+            int profit = arr[i] - min;
+            maxProfit = Math.max(profit, maxProfit);
+        }
+
+        return maxProfit;
+    }
+
+    public static int maxProfit_3(int[] prices) {
+        int buy = 0;
+        int sell = 1;
+
+        int maxProfit = 0;
+
+        while (sell < prices.length) {
+            if (prices[sell] > prices[buy]) {
+                int profit = prices[sell] - prices[buy];
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buy = sell;
+            }
+            sell++;
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        System.out.println(maxProfit_1(prices));
+        System.out.println(maxProfit_2(prices));
+        System.out.println(maxProfit_3(prices));
+    }
+}
