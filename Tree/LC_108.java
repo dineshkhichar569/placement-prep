cd1e8317aa3b77d15656d3cff56f0bcfe6a731fd
dineshkhichar569 <dineshkhichar569@gmail.com>
2026-04-28 20:08:12 +0530

    reattempted leetcode 108

--- Tree/LC_108.java
+++ Tree/LC_108.java
@@ -15,7 +15,7 @@ public class LC_108 {
 
         return node;
     }
-    public static _TreeNode sortedArratToBST(int[] arr){
+    public static _TreeNode sortedArrayToBST(int[] arr){
         if(arr.length == 0){
             return null;
         }
@@ -23,9 +23,31 @@ public class LC_108 {
         return bst(arr, 0, arr.length-1);
     }
 
+
+    public static _TreeNode bst2(int[] arr, int start, int end){
+        if(start > end) {
+            return null;
+        }
+        int mid = start + (end - start)/2;
+
+        _TreeNode node = new _TreeNode(arr[mid]);
+
+        node.left = bst2(arr, start, mid - 1);
+        node.right = bst2(arr, mid + 1, end);
+
+        return node;
+    }
+    public static _TreeNode sortedArrayToBST2(int[] arr) {
+        if(arr.length == 0){
+            return null;
+        }
+
+        return bst2(arr, 0, arr.length-1);
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7};
 
-        System.out.println(sortedArratToBST(arr));
+        System.out.println(sortedArrayToBST2(arr));
     }
 }
